787e9d7683b09d6970617d5ae8483c6e118340a6
Brajneesh Kumar <Brajneesh Kumar>
2018-06-28 18:31:13 +0530

    modified time to utc in Benkow bot

--- intelmq/bots/parsers/benkow/parser.py
+++ intelmq/bots/parsers/benkow/parser.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from datetime import timedelta
 
 from intelmq.lib import utils
 from intelmq.lib.bot import Bot
@@ -13,18 +12,17 @@ class BenkowParserBot(Bot):
         report = self.receive_message()
         raw_report = utils.base64_decode(report["raw"])
         feed_list = raw_report.split('\n')
-        for feed in feed_list[1:-1]:         
-            columns = (feed.replace('";;"','";"";"').split('";"'))[1:5]
+        for feed in feed_list[1:-1]:
+            columns = (feed.replace('";;"', '";"";"').split('";"'))[1:5]
             event = self.new_event(report)
             event.add('malware.name', columns[0])
             if(columns[1].startswith('http://')):
                 event.add('source.url', columns[1])
             else:
                 event.add('source.url', 'http://' + columns[1])
-            event.add('source.ip', columns[2], raise_failure = False)
+            event.add('source.ip', columns[2], raise_failure=False)
             event.add('classification.type', 'malware')
-            event.add('time.source', (datetime.datetime.strptime(columns[3].replace('";',''), '%d-%m-%Y') \
-                                                      + timedelta(minutes=330)).isoformat())
+            event.add('time.source', (datetime.datetime.strptime(columns[3].replace('";', ''), '%d-%m-%Y')).isoformat() + 'UTC')
             event.add('raw', feed)
             self.send_message(event)
 
@@ -32,4 +30,3 @@ class BenkowParserBot(Bot):
 
 
 BOT = BenkowParserBot
-
--- intelmq/tests/bots/parsers/benkow/test_parser.py
+++ intelmq/tests/bots/parsers/benkow/test_parser.py
@@ -18,27 +18,27 @@ EXAMPLE_REPORT = {"feed.url": "http://benkow.cc/export.php",
                   "time.observation": "2018-06-28T08:11:43+00:00"
                   }
 
-EXAMPLE_EVENT = [{ "__type": "Event",
-                   'feed.name': 'Benkow Feed',
-                   'feed.url': 'http://benkow.cc/export.php',
-                   "classification.type": "malware",
-                   'malware.name': 'pony',
-                   'source.ip': '103.63.2.250',
-                   'source.url': 'http://www.faridatiannery.com/panel/admin.php',
-                   'time.source': '2017-08-26T00:00:00+00:00',
-                   'raw': 'IjEiOyJwb255IjsiaHR0cDovL3d3dy5mYXJpZGF0aWFubmVyeS5jb20vcGFuZWwvY'
-                          'WRtaW4ucGhwIjsiMTAzLjYzLjIuMjUwIjsiMjYtMDgtMjAxNyI7',
+EXAMPLE_EVENT = [{"__type": "Event",
+                  'feed.name': 'Benkow Feed',
+                  'feed.url': 'http://benkow.cc/export.php',
+                  "classification.type": "malware",
+                  'malware.name': 'pony',
+                  'source.ip': '103.63.2.250',
+                  'source.url': 'http://www.faridatiannery.com/panel/admin.php',
+                  'time.source': '2017-08-26T00:00:00+00:00',
+                  'raw': 'IjEiOyJwb255IjsiaHR0cDovL3d3dy5mYXJpZGF0aWFubmVyeS5jb20vcGFuZWwvY'
+                         'WRtaW4ucGhwIjsiMTAzLjYzLjIuMjUwIjsiMjYtMDgtMjAxNyI7',
                   },
-                  { "__type": "Event",                   
-                    'feed.name': 'Benkow Feed',
-                    'feed.url': 'http://benkow.cc/export.php',
-                    "classification.type": "malware",
-                    'malware.name': 'zeus fork', 
-                    'source.url': 'http://hutrnadhi.com/off/www.php?m=login', 
-                    'source.ip': '119.28.43.178', 
-                    'time.source': '2017-08-26T00:00:00+00:00',
-                    'raw': 'IjIiOyJaZXVzIGZvcmsiOyJodHRwOi8vaHV0cm5hZGhpLmNvbS9vZmYvd3d3LnBocD9'
-                           'tPWxvZ2luIjsiMTE5LjI4LjQzLjE3OCI7IjI2LTA4LTIwMTciOw==',                  
+                 {"__type": "Event",
+                  'feed.name': 'Benkow Feed',
+                  'feed.url': 'http://benkow.cc/export.php',
+                  "classification.type": "malware",
+                  'malware.name': 'zeus fork',
+                  'source.url': 'http://hutrnadhi.com/off/www.php?m=login',
+                  'source.ip': '119.28.43.178',
+                  'time.source': '2017-08-26T00:00:00+00:00',
+                  'raw': 'IjIiOyJaZXVzIGZvcmsiOyJodHRwOi8vaHV0cm5hZGhpLmNvbS9vZmYvd3d3LnBocD9'
+                         'tPWxvZ2luIjsiMTE5LjI4LjQzLjE3OCI7IjI2LTA4LTIwMTciOw==',
                   }]
 
 
@@ -57,6 +57,7 @@ class TestBenkowParserBot(test.BotTestCase, unittest.TestCase):
         self.run_bot()
         self.assertMessageEqual(0, EXAMPLE_EVENT[0])
         self.assertMessageEqual(1, EXAMPLE_EVENT[1])
+
 
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
