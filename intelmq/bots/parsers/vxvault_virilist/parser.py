b47f37cd3a7c74895572dea7d853f8c7e77c7b7a
Brajneesh Kumar <Brajneesh Kumar>
2019-01-21 15:58:03 +0530

    add vxvault_link in feeds

--- intelmq/bots/parsers/vxvault_virilist/parser.py
+++ intelmq/bots/parsers/vxvault_virilist/parser.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup as bs
-from datetime import datetime
 
 from intelmq.lib import utils
 from intelmq.lib.bot import Bot
@@ -15,7 +14,6 @@ class VXVaultViriListParserBot(Bot):
 
         soup = bs(raw_report, 'html.parser')
         feed_list = soup.find_all('tr')[1:]
-        current_year = datetime.now().year
 
         for feed in feed_list:
             data = feed.find_all('td')
@@ -24,11 +22,7 @@ class VXVaultViriListParserBot(Bot):
             event.add('source.url', 'http://' + data[1].text.split('[D] ')[1])
             event.add('source.ip', data[3].text.strip())
             event.add('malware.hash.md5', data[2].text)
-            feed_time = datetime.strptime(data[0].text, '%m-%d').replace(year=current_year)
-            if feed_time < datetime.now():
-                event.add('time.source', feed_time.isoformat() + 'UTC')
-            else:
-                event.add('time.source', (feed_time.replace(year=current_year - 1)).isoformat() + 'UTC')
+            event.add('extra.vxvault_link', 'http://vxvault.net/' + data[0].find('a').get('href'))
             event.add('classification.type', 'malware')
             event.add('raw', feed)
             self.send_message(event)
