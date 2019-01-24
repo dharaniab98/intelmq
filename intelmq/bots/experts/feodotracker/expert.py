ce553bd526c02d4a56aeb11ee815a4677539e13a
Brajneesh Kumar <Brajneesh Kumar>
2019-01-24 10:24:41 +0530

    Feodo Tracker Expert bot added

---
+++ intelmq/bots/experts/feodotracker/expert.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup as bs
+from datetime import datetime as dt
+
+from intelmq.lib.bot import Bot
+from intelmq.lib import utils
+
+
+class FeodoTrackerExpertBot(Bot):
+
+    def process(self):
+        event = self.receive_message()
+        ip = event.get("source.ip")
+        web_text = requests.get("https://feodotracker.abuse.ch/browse/host/" + ip).text
+        web_soup = bs(web_text, 'html.parser')
+        feeds = web_soup.find_all('table')[1]
+        for feed in feeds.find_all('tr')[1:]:
+            data = feed.find_all('td')
+            event.add('malware.hash.md5', data[1].text, overwrite=True)
+            event.add('source.port', data[4].text, overwrite=True)
+            event.add('time.source', (dt.strptime(data[0].text, '%Y-%m-%d %H:%M:%S')).isoformat() + 'UTC', overwrite=True)
+            self.send_message(event)
+        self.acknowledge_message()
+
+
+BOT = FeodoTrackerExpertBot
---
+++ intelmq/tests/bots/experts/feodotracker/test_expert.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import intelmq.lib.test as test
+from intelmq.bots.experts.feodotracker.expert import FeodoTrackerExpertBot
+
+EXAMPLE_INPUT = {"__type": "Event",
+                 "feed.url": "https://feodotracker.abuse.ch/browse",
+                 "feed.provider": "abuse.ch",
+                 "time.observation": "2019-01-24T04:00:50+00:00",
+                 "source.ip": "186.75.241.230",
+                 "status": "Online",
+                 "time.source": "2019-01-23T09:20:10+00:00",
+                 "classification.type": "c&c",
+                 "malware.name": "heodo",
+                 "source.asn": 11556
+                 }
+EXAMPLE_OUTPUT = {"__type": "Event",
+                  "feed.url": "https://feodotracker.abuse.ch/browse",
+                  "feed.provider": "abuse.ch",
+                  "time.observation": "2019-01-24T04:00:50+00:00",
+                  "source.ip": "186.75.241.230",
+                  "status": "Online",
+                  "time.source": "2019-01-23T09:20:10+00:00",
+                  "classification.type": "c&c",
+                  "malware.name": "heodo",
+                  "source.asn": 11556,
+                  "source.port": 80,
+                  "malware.hash.md5": "8a831c8be1460a0da440b3b8c0087db5"
+                  }
+
+
+class TestFeodoTrackerExpertBot(test.BotTestCase, unittest.TestCase):
+
+    @classmethod
+    def set_bot(cls):
+        cls.bot_reference = FeodoTrackerExpertBot
+
+    def test_event(self):
+        self.input_message = EXAMPLE_INPUT
+        self.run_bot()
+        self.assertMessageEqual(-1, EXAMPLE_OUTPUT)
+
+
+if __name__ == '__main__':
+    unittest.main
