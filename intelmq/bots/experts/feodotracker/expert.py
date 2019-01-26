249443c180bd60cafd3183445ad51e5c3a4ff4c5
Brajneesh Kumar <Brajneesh Kumar>
2019-01-26 19:40:56 +0530

    FIX: Malware Sample table existance

--- intelmq/bots/experts/feodotracker/expert.py
+++ intelmq/bots/experts/feodotracker/expert.py
@@ -14,13 +14,14 @@ class FeodoTrackerExpertBot(Bot):
         ip = event.get("source.ip")
         web_text = requests.get("https://feodotracker.abuse.ch/browse/host/" + ip).text
         web_soup = bs(web_text, 'html.parser')
-        feeds = web_soup.find_all('table')[1]
-        for feed in feeds.find_all('tr')[1:]:
-            data = feed.find_all('td')
-            event.add('malware.hash.md5', data[1].text, overwrite=True)
-            event.add('source.port', data[4].text, overwrite=True)
-            event.add('time.source', (dt.strptime(data[0].text, '%Y-%m-%d %H:%M:%S')).isoformat() + 'UTC', overwrite=True)
-            self.send_message(event)
+        feeds = web_soup.find_all('table')
+        if len(feeds) > 1:
+            for feed in feeds[1].find_all('tr')[1:]:
+                data = feed.find_all('td')
+                event.add('malware.hash.md5', data[1].text, overwrite=True)
+                event.add('source.port', data[4].text, overwrite=True)
+                event.add('time.source', (dt.strptime(data[0].text, '%Y-%m-%d %H:%M:%S')).isoformat() + 'UTC', overwrite=True)
+                self.send_message(event)
         self.acknowledge_message()
 
 
